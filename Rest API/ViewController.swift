fb4fb12d0ee5292f40009f4e6f3e917a8fe04b16
Victor <victor.a.vieira@hotmail.com>
2022-02-06 13:11:28 -0400

    Initial Commit

---
+++ Rest API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rest API
+//
+//  Created by Victor Vieira on 06/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
