0d48ac5cf11d573da49abf3a0ba2216a94529d2d
Victor <victor.a.vieira@hotmail.com>
2022-02-07 17:38:47 -0400

    add movieTableViewCell and make layout in storyboard

---
+++ Rest API/MovieTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieTableViewCell.swift
+//  Rest API
+//
+//  Created by Victor Vieira on 07/02/22.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+}
--- Rest API/ViewController.swift
+++ Rest API/ViewController.swift
@@ -8,15 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var apiService = ApiService()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        apiService.getPopularMoviesData { (result) in
-            print(result)
-        }
         
     }
 
