51ba339fea5e9024dafcb3680308dfe31bb4a2ce
Victor <victor.a.vieira@hotmail.com>
2022-02-24 09:32:03 -0400

    add ScrollView in movieDetails

--- Rest API/View/MovieDetailsView.swift
+++ Rest API/View/MovieDetailsView.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class MovieDetailsView: UIView {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+    
     var moviePoster: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -67,36 +79,49 @@ class MovieDetailsView: UIView {
     }
     
     private func setupViews() {
-        addSubview(moviePoster)
-        addSubview(movieYear)
-        addSubview(movieRate)
-        addSubview(movieOverview)
-        addSubview(buttonBuy)
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(moviePoster)
+        contentView.addSubview(movieYear)
+        contentView.addSubview(movieRate)
+        contentView.addSubview(movieOverview)
+        contentView.addSubview(buttonBuy)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             
-            moviePoster.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            moviePoster.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            moviePoster.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20),
+            moviePoster.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             moviePoster.heightAnchor.constraint(equalToConstant: 300),
             moviePoster.widthAnchor.constraint(equalToConstant: 200),
             
-            buttonBuy.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
-            buttonBuy.trailingAnchor.constraint(equalTo: trailingAnchor),
-            buttonBuy.leadingAnchor.constraint(equalTo: leadingAnchor),
+            buttonBuy.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+            buttonBuy.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            buttonBuy.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             buttonBuy.heightAnchor.constraint(equalToConstant: 50),
             
             movieOverview.bottomAnchor.constraint(equalTo: buttonBuy.topAnchor, constant: -20),
-            movieOverview.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            movieOverview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            movieOverview.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            movieOverview.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             movieOverview.topAnchor.constraint(equalTo: moviePoster.bottomAnchor, constant: 20),
 
-            movieYear.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            movieYear.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             movieYear.leadingAnchor.constraint(equalTo: moviePoster.trailingAnchor, constant: 20),
-            movieYear.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            movieYear.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20),
 
-            movieRate.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            movieRate.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             movieRate.leadingAnchor.constraint(equalTo: moviePoster.trailingAnchor, constant: 20),
             movieRate.topAnchor.constraint(equalTo: movieYear.bottomAnchor, constant: 20)
         ])
